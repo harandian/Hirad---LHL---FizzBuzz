3c55bd01b4e04ec283ddfcd44d69c33e8b9ed4e3
Hirad <hharandian@gmail.com>
2017-05-09 23:49:27 -0700

    Initial Commit

---
+++ FizzBuzz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FizzBuzz
+//
+//  Created by Hirad on 2017-05-09.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
